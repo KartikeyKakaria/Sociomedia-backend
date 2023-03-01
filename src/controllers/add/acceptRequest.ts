f814589c012828d862720e10eedbaa14f97e79f7
KartikeyKakaria <kartikeykakaria2@gmail.com>
2023-03-01 14:07:48 +0530

    Added route to accept friend requests, will now work on uploading posts

---
+++ src/controllers/add/acceptRequest.ts
@@ -0,0 +1,41 @@
+import { Request, Response, RequestHandler } from 'express';
+import getFriendRequests from "../../lib/getFriendRequests";
+import USER from "../../models/User";
+import friendReq from "../../models/friendReq";
+import { msgResponse } from '../../lib/Classes';
+
+const acceptRequest:RequestHandler = async (req:Request, res:Response)=>{
+    const rep = new msgResponse(false, "accepted successfully")
+    const { _id } = JSON.parse(req.params.user)
+    const { requestId } = req.body;
+    const requests = await getFriendRequests(requestId);
+    if(!requests){
+        rep.changeMessage("Server error")
+        return res.status(500).json(rep)
+    }
+    if(!(requests as string[]).includes(_id)){
+        rep.changeMessage("No pending friend request from the user");
+        return res.status(402).json(rep)
+    }
+    
+    try{
+        await friendReq.deleteOne({from:requestId, to:_id});
+        await USER.updateOne({_id:_id}, {
+            $push:{
+                friends:requestId
+            }
+        })
+        await USER.updateOne({_id:requestId},{
+            $push:{
+                friends:_id
+            }
+        })
+        rep.changeStats(true);
+        return res.status(201).json(rep);
+    }catch(error){
+        rep.changeMessage(`${error}`);
+        return res.status(500).json(rep)
+    }
+}
+
+export default acceptRequest;
--- src/routers/add.ts
+++ src/routers/add.ts
@@ -3,7 +3,9 @@ const addRouter = Router();
 import userAuth from '../middleware/authUser';
 import addFollowing from '../controllers/add/follow';
 import friendReq from '../controllers/add/friendRequest';
+import acceptReq from '../controllers/add/acceptRequest';
 
 addRouter.post('/follow', userAuth, addFollowing);
 addRouter.post('/friend', userAuth, friendReq)
+addRouter.post('/accept',userAuth, acceptReq)
 export default addRouter;
