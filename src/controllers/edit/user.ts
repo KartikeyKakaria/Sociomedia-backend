b58a097159023cfd78a30de33d1873cf2fb5a497
KartikeyKakaria <81517323+KartikeyKakaria@users.noreply.github.com>
2023-04-08 20:44:11 +0530

    added route for editing user details

--- src/controllers/edit/user.ts
+++ src/controllers/edit/user.ts
@@ -1,1 +1,34 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, RequestHandler } from "express";
+import USER from "../../models/User";
+import { msgResponse } from "../../lib/Classes";
+const editUser: RequestHandler = async (req: Request, res: Response) => {
+    const { _id, name, email, age, DOB, gender, number } = JSON.parse(req.params.user);
+    const { newName, newAge, newEmail, newDOB, newGender, newNumber } = req.body;
+    console.log(newName)
+    const resp = new msgResponse(false, "Data updated successfully")
+    if (!newName && !newAge && !newEmail && !newDOB && !newGender && !newNumber) {
+        resp.changeMessage("No change")
+        return res.status(402).json(resp)
+    }
+    try {
+        await USER.updateOne({ _id }, {
+            $set: {
+                name: newName || name,
+                age: newAge || age,
+                email: newEmail || email,
+                DOB: newDOB || DOB,
+                gender: newGender || gender,
+                number: newNumber || number,
+            }
+        })
+        resp.changeStats(true);
+        return res.status(201).json(resp)
+
+
+    } catch (error) {
+        resp.changeMessage(`${error}`);
+        return res.status(500).json(resp)
+    }
+}
+
+export default editUser;
--- src/routers/edit.ts
+++ src/routers/edit.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import authUser from "../middleware/authUser";
-
+import editUser from "../controllers/edit/user";
 const editRouter = Router();
 
-editRouter.post('/me', authUser);
+editRouter.post('/me', authUser, editUser);
 
 
 export default editRouter;
